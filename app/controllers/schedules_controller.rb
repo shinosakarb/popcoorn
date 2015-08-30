8e46cd3754325291347dbe160b0a6ebad97f9488
FUNABARA Masao <masao@masoo.jp>
2015-08-30 16:20:21 +0900

    add schedule show page.

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -2,4 +2,8 @@ class SchedulesController < ApplicationController
   def index
     @schedules = Schedule.includes(:movie)
   end
+
+  def show
+    @schedule = Schedule.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :schedules, only: [:index]
+  resources :schedules, only: [:index, :show]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
